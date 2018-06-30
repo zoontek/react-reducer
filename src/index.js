b6757650136df0e2d57a931a5873874b5364499c
zoontek <zoontek@gmail.com>
2018-06-30 20:29:57 +0200

    initialState now can be anything

--- src/index.js
+++ src/index.js
@@ -6,32 +6,33 @@ export const checkExhaustiveness = (action: { type: empty }) => {
   throw new Error(`Unhandled action of type ${action.type}`);
 };
 
-export type RenderParam<State, Action> = {|
-  state: State,
-  send: Action => void,
-|};
-
 type Props<State, Action> = {|
   initialState: State,
   reducer: (State, Action) => State,
-  render: (RenderParam<State, Action>) => React$Node,
+  render: ({|
+    state: State,
+    send: Action => void,
+  |}) => React$Node,
 |};
 
 export class Component<
-  State: {},
+  State,
   Action: { type: $Subtype<string> },
-> extends React.Component<Props<State, Action>, State> {
-  state: State;
-
+> extends React.Component<Props<State, Action>, {| reducerState: State |}> {
   constructor(props: Props<State, Action>) {
     super(props);
-    this.state = props.initialState;
+    this.state = {
+      reducerState: props.initialState,
+    };
   }
 
   render() {
     return this.props.render({
-      state: this.state,
-      send: action => this.setState(state => this.props.reducer(state, action)),
+      state: this.state.reducerState,
+      send: action =>
+        this.setState(state => ({
+          reducerState: this.props.reducer(state.reducerState, action),
+        })),
     });
   }
 }
